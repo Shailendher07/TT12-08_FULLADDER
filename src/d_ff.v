e1a0fb9e9dcc28a68ca65089d6b29d319af0d4a9
Shailendher07 <122807384+Shailendher07@users.noreply.github.com>
2025-06-20 10:43:50 +0530

    d_flipflop

---
+++ src/d_ff.v
@@ -0,0 +1,19 @@
+module d_ff(clk , rst , d, out);
+input clk , rst , d;
+output reg out;
+
+always @(posedge clk or posedge rst)
+begin
+
+if (rst)
+begin
+out <= 1;
+end
+
+else
+begin
+out <= d;
+end
+
+end
+endmodule
